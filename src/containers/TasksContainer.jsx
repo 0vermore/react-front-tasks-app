f5097facbcff4b83a36c5747aebc59f4cd11ec38
Daniil Orlov <0vermore@ukr.net>
2021-02-15 16:35:35 +0200

    v1

--- src/containers/TasksContainer.jsx
+++ src/containers/TasksContainer.jsx
@@ -9,7 +9,7 @@ class TasksContainer extends Component {
 
 	getAuthToken() {
 		var config = {
-			baseURL: 'http://localhost:4000' || 'https://tasks-api-rails.herokuapp.com',
+			baseURL: 'https://tasks-api-rails.herokuapp.com',
 			timeout: 10000,
 			headers: {'Authorization': 'Bearer ' + localStorage.getItem('jwt'),
 			'Access-Control-Allow-Origin': '*'}
